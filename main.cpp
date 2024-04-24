83f0fef57687eb09d2f64b8407b1a5cfee64db4d
Daniel Raspa <daniel.raspa@studio.unibo.it>
2024-04-24 17:29:30 +0200

    Add reading data from a file

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include "statistics.hpp"
 
 #include <random>
-#include<iostream>
+#include <iostream>
+#include <fstream>
 
 int main () {
   pf::Sample data{};
-  std::random_device r;
-  std::default_random_engine eng(r());
-  std::normal_distribution<double> dist;
+  std::ifstream in_file("data.txt");
 
-  for(int i = 1; i < 1000000; i++) {
-    data.add(dist(eng));
+  if (in_file.is_open()) {
+    std::cout << "The file has been opened" << '\n';
+  }
+
+  double x;
+  while (in_file >> x) {
+    data.add(x);
   }
 
   const auto stat{data.statistics()};
+
   std::cout << "Mean: " << stat.mean << '\n'
-            << "Sigma: " << stat.sigma << '\n';
+            << "Sigma: " << stat.sigma << '\n'
+            << "Mean error: " << stat.mean_err << '\n'
+            << "Median: " << stat.median << '\n';
+
+  std::ofstream out_file("result.txt");
+
+  if (out_file.is_open()) {
+    out_file << stat.mean << '\n'
+              << stat.sigma << '\n'
+              << stat.mean_err << '\n'
+              << stat.median << '\n';
+    std::cout << "Results has been saved in result.txt" << '\n';
+  }
 }  
